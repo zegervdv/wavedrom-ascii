a187cb1020148d1197dc0ce95a3de6734ff7bc3e
Zeger Van de Vannet <747627+zegervdv@users.noreply.github.com>
2022-03-28 19:17:09 +0200

    support long fields spanning multiple levels

--- wavedrom_ascii/__init__.py
+++ wavedrom_ascii/__init__.py
@@ -3,30 +3,32 @@ from collections import deque, defaultdict
 from math import ceil
 import json5
 
+
 class Waveform:
     symbol_top = {
-        'rise': "┌────",
-        'fall': "┐    ",
-        'low' : "     ",
-        'high': "─────",
+        "rise": "┌────",
+        "fall": "┐    ",
+        "low": "     ",
+        "high": "─────",
     }
     symbol_bot = {
-        'rise': "┘    ",
-        'fall': "└────",
-        'low' : "─────",
-        'high': "     ",
+        "rise": "┘    ",
+        "fall": "└────",
+        "low": "─────",
+        "high": "     ",
     }
 
     signals = {
-        '0': 'low',
-        '1': 'high',
+        "0": "low",
+        "1": "high",
     }
+
     def __init__(self):
         self._cycles = []
 
     def parse_waveform(self, wave, data=None):
-        if wave[0] == 'p':
-            self._cycles = [('rise', 'fall') for _ in range(len(wave))]
+        if wave[0] == "p":
+            self._cycles = [("rise", "fall") for _ in range(len(wave))]
         else:
             result = []
             waves = deque(wave)
@@ -35,14 +37,18 @@ class Waveform:
             while waves:
                 cycle = waves.popleft()
 
-                if cycle == '.':
+                if cycle == ".":
                     posedge = last_cycle[1]
                     negedge = last_cycle[1]
                 else:
-                    if (last_cycle is None or last_cycle[1] == 'low') and cycle == '1':
-                        posedge = 'rise'
-                    elif last_cycle is not None and last_cycle[1] == 'high' and cycle == '0':
-                        posedge = 'fall'
+                    if (last_cycle is None or last_cycle[1] == "low") and cycle == "1":
+                        posedge = "rise"
+                    elif (
+                        last_cycle is not None
+                        and last_cycle[1] == "high"
+                        and cycle == "0"
+                    ):
+                        posedge = "fall"
                     else:
                         posedge = self.signals.get(cycle, None)
 
@@ -55,7 +61,7 @@ class Waveform:
 
     @property
     def topline(self):
-        topline = ''
+        topline = ""
         for posedge, negedge in self._cycles:
             topline += self.symbol_top.get(posedge, "     ")
             topline += self.symbol_top.get(negedge, "     ")
@@ -67,54 +73,53 @@ class Waveform:
 
     @property
     def botline(self):
-        botline = ''
+        botline = ""
         for posedge, negedge in self._cycles:
             botline += self.symbol_bot.get(posedge, "     ")
             botline += self.symbol_bot.get(negedge, "     ")
         return botline
 
 
-
 class VectorWaveform(Waveform):
-    change_top = '╥'
-    change_mid = '║'
-    change_bot = '╨'
+    change_top = "╥"
+    change_mid = "║"
+    change_bot = "╨"
 
     def parse_waveform(self, wave, data):
         prev = None
         last_cycle = None
         waves = deque(wave)
-        data = data.split(' ')
+        data = data.split(" ")
         while waves:
             cycle = waves.popleft()
 
-            value = ''
-            if cycle == '.':
+            value = ""
+            if cycle == ".":
                 posedge = last_cycle[1]
                 negedge = posedge
             else:
-                if cycle == 'x':
-                    if last_cycle and last_cycle[0] != 'x':
-                        posedge = 'changex'
+                if cycle == "x":
+                    if last_cycle and last_cycle[0] != "x":
+                        posedge = "changex"
                     else:
-                        posedge = 'x'
-                    negedge = 'x'
+                        posedge = "x"
+                    negedge = "x"
                 else:
-                    if last_cycle is None and cycle == '.':
-                        posedge = 'hold'
-                        negedge = 'hold'
-                        value = ''
+                    if last_cycle is None and cycle == ".":
+                        posedge = "hold"
+                        negedge = "hold"
+                        value = ""
                     elif last_cycle is None:
-                        posedge = 'change'
-                        negedge = 'hold'
+                        posedge = "change"
+                        negedge = "hold"
                         value = data.pop(0)
-                    elif cycle in '23456789':
-                        posedge = 'change'
-                        negedge = 'hold'
+                    elif cycle in "23456789":
+                        posedge = "change"
+                        negedge = "hold"
                         value = data.pop(0)
                     else:
-                        posedge = 'hold'
-                        negedge = 'hold'
+                        posedge = "hold"
+                        negedge = "hold"
 
             prev = cycle
             last_cycle = (posedge, negedge, value)
@@ -122,9 +127,9 @@ class VectorWaveform(Waveform):
 
     @property
     def topline(self):
-        output = ''
+        output = ""
         for posedge, _, _ in self._cycles:
-            if posedge in {'change', 'changex'}:
+            if posedge in {"change", "changex"}:
                 output += f"{self.change_top}────"
             else:
                 output += "─────"
@@ -134,9 +139,9 @@ class VectorWaveform(Waveform):
 
     @property
     def botline(self):
-        output = ''
+        output = ""
         for posedge, _, _ in self._cycles:
-            if posedge in {'change', 'changex'}:
+            if posedge in {"change", "changex"}:
                 output += f"{self.change_bot}────"
             else:
                 output += "─────"
@@ -146,51 +151,53 @@ class VectorWaveform(Waveform):
 
     @property
     def midline(self):
-        output = ''
+        output = ""
         for posedge, negedge, value in self._cycles:
-            if posedge == 'x':
+            if posedge == "x":
                 output += "XXXXX"
-            elif posedge == 'change':
+            elif posedge == "change":
                 output += f"{self.change_mid} {value[0:3]:3s}"
-            elif posedge == 'changex':
+            elif posedge == "changex":
                 output += f"{self.change_mid}XXXX"
             else:
                 output += "     "
 
-            if negedge == 'x':
+            if negedge == "x":
                 output += "XXXXX"
-            elif posedge == 'change':
+            elif posedge == "change":
                 output += f"{value[3:8]:5s}"
             else:
                 output += "     "
 
         return output
 
+
 class WavedromASCII:
     vector_symbols = {
-        'x',
-        '2',
-        '3',
-        '4',
-        '5',
-        '6',
-        '7',
-        '8',
-        '9',
+        "x",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
     }
+
     def __init__(self):
         self._waves = {}
 
     @classmethod
     def from_dict(cls, data):
         wave = cls()
-        for item in data['signal']:
-            wave.add_wave(item['name'], item['wave'], item.get('data', None))
+        for item in data["signal"]:
+            wave.add_wave(item["name"], item["wave"], item.get("data", None))
         return wave
 
     @classmethod
     def from_json(cls, file):
-        with open(file, 'r') as f:
+        with open(file, "r") as f:
             data = json5.load(f)
             wave = cls.from_dict(data)
             return wave
@@ -218,7 +225,7 @@ class WavedromASCII:
                 midline = f"  {name:{max_key}s}  "
                 botline = f"  {' ':{max_key}s}  "
             else:
-                midline = ''
+                midline = ""
                 botline = f"  {name:{max_key}s}  "
 
             midline += waveform.midline
@@ -228,24 +235,25 @@ class WavedromASCII:
             if isinstance(waveform, VectorWaveform):
                 output.append(midline)
             output.append(botline)
-        return '\n'.join(output)
+        return "\n".join(output)
 
 
 class BitfieldASCII:
     fieldsize = 32
+
     def __init__(self):
         self._fields = []
 
     @classmethod
     def from_dict(cls, data):
         bitfield = cls()
-        for item in data['reg']:
-            bitfield.add_field(item['name'], item['bits'])
+        for item in data["reg"]:
+            bitfield.add_field(item["name"], item["bits"])
         return bitfield
 
     @classmethod
     def from_json(cls, file):
-        with open(file, 'r') as f:
+        with open(file, "r") as f:
             data = json5.load(f)
             return cls.from_dict(data)
 
@@ -259,13 +267,13 @@ class BitfieldASCII:
         return name
 
     def __str__(self):
-        lines = int(ceil(self.bitsize()/self.fieldsize))
+        lines = int(ceil(self.bitsize() / self.fieldsize))
         msb = lines * self.fieldsize
 
         output = []
         if self.bitsize() < msb:
             pad = msb - self.bitsize()
-            fields = [(pad, '')] + list(reversed(self._fields))
+            fields = [(pad, "")] + list(reversed(self._fields))
         else:
             pad = 0
             fields = reversed(self._fields)
@@ -273,11 +281,13 @@ class BitfieldASCII:
         field_ranges = []
         offset = 0
         for size, data in fields:
-            field_ranges.append((
-                data,
-                msb - offset - 1,
-                msb - offset - size,
-            ))
+            field_ranges.append(
+                (
+                    data,
+                    msb - offset - 1,
+                    msb - offset - size,
+                )
+            )
             offset += size
 
         digits = 1
@@ -287,12 +297,30 @@ class BitfieldASCII:
         field_lines = defaultdict(list)
         index = 0
         for field in reversed(field_ranges):
-            if field[1] < (index+1) * self.fieldsize:
+            if field[1] < (index + 1) * self.fieldsize:
                 field_lines[index].append(field)
-            elif field[2] < (index+1) * self.fieldsize:
-                field_lines[index].append((field[0],  (index+1) * self.fieldsize - 1, field[2]))
-                index += 1
-                field_lines[index].append((field[0], field[1], index * self.fieldsize))
+            elif field[2] < (index + 1) * self.fieldsize:
+                field_lines[index].append(
+                    (field[0], (index + 1) * self.fieldsize - 1, field[2])
+                )
+                remain = (
+                    field[1] - field[2] - ((index + 1) * self.fieldsize - 1 - field[2])
+                )
+                for i in range(remain // self.fieldsize):
+                    index += 1
+                    field_lines[index].append(
+                        (
+                            field[0],
+                            (index + 1) * self.fieldsize - 1,
+                            (index) * self.fieldsize,
+                        ),
+                    )
+                    remain -= self.fieldsize
+                if remain > 0:
+                    index += 1
+                    field_lines[index].append(
+                        (field[0], field[1], index * self.fieldsize)
+                    )
             else:
                 index += 1
                 field_lines[index].append(field)
@@ -313,14 +341,14 @@ class BitfieldASCII:
                 else:
                     bit_numbers[0].append(f"{high:>2d}")
                 if first:
-                    topline.append('┌')
-                    midline.append('│')
-                    botline.append('└')
+                    topline.append("┌")
+                    midline.append("│")
+                    botline.append("└")
                     first = False
                 else:
-                    topline.append('┬')
-                    midline.append('│')
-                    botline.append('┴')
+                    topline.append("┬")
+                    midline.append("│")
+                    botline.append("┴")
                 chars = size * 2 - 1
                 if isinstance(data, int):
                     bit = f"{data:0{size}b}"
@@ -328,7 +356,7 @@ class BitfieldASCII:
                 else:
                     name = f"{data:^{chars}s}"[0:chars]
                 for i in range(digits):
-                    bit_numbers[i].extend(['  ' * (size - 2)])
+                    bit_numbers[i].extend(["  " * (size - 2)])
                 if size > 1:
                     if digits > 1:
                         bit = f"{low:>{digits}d}"
@@ -336,22 +364,21 @@ class BitfieldASCII:
                             bit_numbers[i].append(f" {bit[i]}")
                     else:
                         bit_numbers[0].append(f"{low:>2d}")
-                topline.append('─')
-                topline.extend(['┬─' * (size - 1)])
+                topline.append("─")
+                topline.extend(["┬─" * (size - 1)])
                 midline.append(self.format_name(name))
-                botline.append('─')
-                botline.extend(['┴─' * (size - 1)])
-            topline.append('┐')
-            midline.append('│')
-            botline.append('┘')
+                botline.append("─")
+                botline.extend(["┴─" * (size - 1)])
+            topline.append("┐")
+            midline.append("│")
+            botline.append("┘")
 
             for i in range(digits):
-                line_out.append(''.join(bit_numbers[i]))
-            line_out.append(''.join(topline))
-            line_out.append(''.join(midline))
-            line_out.append(''.join(botline))
+                line_out.append("".join(bit_numbers[i]))
+            line_out.append("".join(topline))
+            line_out.append("".join(midline))
+            line_out.append("".join(botline))
 
-            output.append('\n'.join(line_out))
+            output.append("\n".join(line_out))
 
-        return '\n'.join(reversed(output))
-
+        return "\n".join(reversed(output))
